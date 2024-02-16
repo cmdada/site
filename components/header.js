f391527efa23aed2f3aa8c994635d8868fc50216
cmd <ada@ads.software>
2024-02-16 12:49:47 -0700

    better header

--- components/header.js
+++ components/header.js
@@ -1,4 +1,3 @@
-
 class Header extends HTMLElement {
   constructor() {
     super();
@@ -6,17 +5,31 @@ class Header extends HTMLElement {
 
   connectedCallback() {
     this.innerHTML = `
-		    <nav>
-		        <a href="index.html"><i class="fa-solid fa-house"></i></a>
-		        <a href="about.html"><i class="fa-solid fa-circle-info"></i></a>
-		        <a href="stuff.html"><i class="fa-solid fa-code"></i></a>
-		        <a href="contact.html"><i class="fa-solid fa-envelope"></i></a>
-		        <a href="blog.html"><i class="fa-solid fa-rss"></i></a>
-		        <a href="adamicro.html">μ</a> 
-		    </nav>
+      <header>
+        <div class="menu-toggle" id="mobile-menu">
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+        <nav class="navbar">
+          <a href="index.html"><i class="fa-solid fa-house"></i></a>
+          <a href="about.html"><i class="fa-solid fa-circle-info"></i></a>
+          <a href="stuff.html"><i class="fa-solid fa-code"></i></a>
+          <a href="contact.html"><i class="fa-solid fa-envelope"></i></a>
+          <a href="blog.html"><i class="fa-solid fa-rss"></i></a>
+          <a href="adamicro.html">μ</a>
+        </nav>
+      </header>
     `;
+
+    // Add event listener for menu toggle
+    const mobileMenuToggle = this.querySelector('#mobile-menu');
+    const navbar = this.querySelector('.navbar');
+
+    mobileMenuToggle.addEventListener('click', () => {
+      navbar.classList.toggle('active');
+    });
   }
 }
 
 customElements.define('header-component', Header);
-
