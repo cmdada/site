23bf13320a963a38b4d1c6b3ca477f83ba3fba32
cmd <ada@ads.software>
2024-02-20 22:39:36 -0700

    now it has a footer

--- components/footer.js
+++ components/footer.js
@@ -1,16 +1,31 @@
-class Header extends HTMLElement {
+class Footer extends HTMLElement {
   constructor() {
     super();
   }
 
   connectedCallback() {
     this.innerHTML = `
+      <style>
+        footer {
+          display: flex;
+          flex-direction: column;
+          position: fixed;
+          bottom: 0;
+          left: 0;
+          width: 100%;
+          align-items: center;
+          justify-content: flex-end;
+          text-align: center;
+          padding-bottom: 0.25vw;
+          padding-top: 0.25vw;
+          background-color: #333333;
+        }
+      </style>
       <footer>
         <code>created by ada t (adas.software)</code>
       </footer>
     `;
-    });
   }
 }
 
-customElements.define('header-component', Header);
+customElements.define('footer-component', Footer);
